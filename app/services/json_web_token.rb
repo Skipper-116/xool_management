bd8fa6d5dd9ed801d0be60ef6278f3a0ada2592f
Roy Chanunkha <roychanunkha@gmail.com>
2022-09-05 16:06:27 +0200

    encrypting user id

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -4,13 +4,14 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  def self.encode(payload, exp = 24.hours.from_now)
+  def self.encode(payload, request_ip, exp = 24.hours.from_now)
+    Rails.logger.info "Client IP Address: #{request_ip}"
     payload[:exp] = exp.to_i
-    JWT.encode(payload, SECRET_KEY)
+    JWT.encode(payload, SECRET_KEY + request_ip)
   end
 
-  def self.decode(token)
-    decoded = JWT.decode(token, SECRET_KEY)[0]
+  def self.decode(token, request_ip)
+    decoded = JWT.decode(token, SECRET_KEY + request_ip)[0]
     HashWithIndifferentAccess.new decoded
   end
 end
