394fa951c8869eaf505f2625ffb0f2ff9175c263
Roy Chanunkha <roychanunkha@gmail.com>
2022-09-05 14:21:19 +0200

    setting user for the current thread

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,18 @@
 
 # this is the main controller
 class ApplicationController < ActionController::API
-  def aunthenticate; end
+  before_action :authorize_request
+
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      decoded = JsonWebToken.decode(header)
+      User.current = User.find(decoded[:person_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_password :recover_password, validations: false
   belongs_to :person
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: { minimum: 6 }
+
+  def self.current
+    Thread.current['current_user']
+  end
+
+  def self.current=(user)
+    Thread.current['current_user'] = user
+  end
 end
