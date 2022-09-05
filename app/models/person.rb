780060e26d58cce96b8c6d2cdb2246ff3ec091ee
Roy Chanunkha <roychanunkha@gmail.com>
2022-09-05 14:42:31 +0200

    user authentication done

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,16 @@
+# frozen_string_literal: true
+
+# This class is used to model a person in the system
 class Person < ApplicationRecord
   belongs_to :gender
+  # has one person_name
+  has_one :person_name, dependent: :destroy
+  # has many person_addresses
+  has_many :person_addresses, dependent: :destroy
+  # has many person_attributes
+  has_many :person_attributes, dependent: :destroy
+
+  def name
+    @name || "#{person_name.given_name} #{person_name.family_name}"
+  end
 end
