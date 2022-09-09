a97dbdba7bb35ca4c35e8be2241f7fc4f99b794f
Roy Chanunkha <roychanunkha@gmail.com>
2022-09-09 15:58:32 +0200

    added a way to create user credentials

--- app/services/person_service.rb
+++ app/services/person_service.rb
@@ -2,23 +2,24 @@
 
 # Person service class
 class PersonService
-  def self.create_person(params)
+  def create_person(params)
     Person.transaction do
       common_create(params)
-      process_student(person, params[:pupils])
+      process_student(person, params[:pupils]) unless params[:pupils]
     end
     person
   end
 
-  def self.common_create(params)
+  def common_create(params)
     person = Person.create(params[:person_params])
+    User.create(username: params[:username], password: SecureRandom.base64[0..7])
     UserRoleService.create_user_roles(person, params[:roles])
   end
-  
-  def self.process_student(person, pupils)
+
+  def process_student(person, pupils)
     pupils.each do |pupil|
       student = common_create(pupil)
-      RelationshipService.create_relationship(person.id, student.id, pupil[:relationship_type])
+      RelationshipService.new.create_relationship(person.id, student.id, pupil[:relationship_type])
     end
   end
 
