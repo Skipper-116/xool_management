945422432b41b240999a41a228195685038dee20
Roy Chanunkha <roychanunkha@gmail.com>
2022-09-09 16:25:46 +0200

    updated person model associations

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,10 +5,14 @@ class Person < VoidableRecord
   belongs_to :gender
   # has one person_name
   has_one :person_name, dependent: :destroy
-  # has many person_addresses
-  has_many :person_addresses, dependent: :destroy
   # has many person_attributes
-  has_many :person_attributes, dependent: :destroy
+  has_many :person_attributes, dependent: :destroy, optional: true
+  # has many user roles
+  has_many :user_roles, dependent: :destroy
+  # has one user
+  has_one :user, dependent: :destroy
+  # has zero or more relationships
+  has_many :relationships, dependent: :destroy, foreign_key: :person_a, optional: true
 
   def name
     @name || "#{person_name.given_name} #{person_name.family_name}"
