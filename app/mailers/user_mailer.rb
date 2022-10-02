b429c77b86ce3454576044979d4031c584869cf5
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-02 09:54:15 +0200

    user mail new user method updated

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,11 @@
 # User mailer class
 class UserMailer < ApplicationMailer
   def new_user
-    @user = params[:user]
-    @password = params[:password]
+    params.compact! each do |param|
+      @user = param[:user]
+      @password = param[:password]
 
-    mail(to: @user.email, subject: 'Welcome to the School Management System')
+      mail(to: @user.person.email, subject: 'Welcome to the School Management System')
+    end
   end
 end
