f9ddf6594dfd4e2e8d22866f54262c54dcaa145c
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-15 19:02:10 +0200

    updates to the gem and files

--- app/models/classroom_student.rb
+++ app/models/classroom_student.rb
@@ -2,4 +2,18 @@ class ClassroomStudent < VoidableRecord
   belongs_to :classroom
   belongs_to :cohort_term
   belongs_to :person
+
+  validates :classroom_id, :cohort_term_id, :person_id, presence: true
+
+  # json output
+  def as_json(options = {})
+    super(options.merge(
+      only: %i[id classroom_id cohort_term_id person_id created_at updated_at],
+      include: {
+        person:,
+        classroom:,
+        cohort_term:
+      }
+    ))
+  end
 end
