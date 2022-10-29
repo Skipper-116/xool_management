36cb4034245be2564f612d4834f6184ebe321884
Roy Chanunkha <roychanunkha@gmail.com>
2022-10-29 11:47:06 +0200

    adding missed files

---
+++ db/migrate/20221029091118_add_reference_columns.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# Add reference columns to the database
+class AddReferenceColumns < ActiveRecord::Migration[7.0]
+  def change
+    # loop through all tables in the database
+    ActiveRecord::Base.connection.tables.each do |table_name|
+      # skip the schema_migrations table
+      next if table_name == 'schema_migrations'
+      next if table_name == 'ar_internal_metadata'
+
+      puts "Adding reference columns to #{table_name}"
+      add_changed_by_column(table_name)
+      add_reference_to_person(table_name)
+    end
+  end
+
+  def down
+    # loop through all tables in the database
+    ActiveRecord::Base.connection.tables.each do |table_name|
+      # skip the schema_migrations table
+      next if table_name == 'schema_migrations'
+
+      remove_changed_by_column(table_name)
+    end
+  end
+
+  private
+
+  def add_changed_by_column(table_name)
+    add_column table_name, :creator, :bigint, null: false, default: 1
+    add_column table_name, :changed_by, :bigint, null: true, default: nil
+    add_column table_name, :voided_by, :bigint, null: true, default: nil
+  end
+
+  def add_reference_to_person(table_name)
+    add_foreign_key table_name, :people, column: :creator, primary_key: :id, index: true
+    add_foreign_key table_name, :people, column: :changed_by, primary_key: :id, index: true
+    add_foreign_key table_name, :people, column: :voided_by, primary_key: :id, index: true
+  end
+
+  def remove_changed_by_column(table_name)
+    remove_column table_name, :creator
+    remove_column table_name, :changed_by
+    remove_column table_name, :voided_by
+  end
+end
